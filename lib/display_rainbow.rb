1d8aee4584b2c24257b93afafec0be845ee09bc5
SagaciousDavid <sagaciousd83@gmail.com>
2018-08-28 09:47:48 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -8,7 +8,7 @@ def display_rainbow(colors)
 
   print "R: #{colors[0]}, "
   print "O: #{colors[1]}, "
-  print "Y: #{colors[2]},"
+  print "Y: #{colors[2]}, "
   print "G: #{colors[3]}, "
   print "B: #{colors[4]}, "
   print "I: #{colors[5]}, "
